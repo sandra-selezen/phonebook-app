c1fa7194af08849d5df0b3990461b404c2132270
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2024-09-28 18:29:14 +0300

    Add redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { ChakraProvider, Spinner } from '@chakra-ui/react';
+
+import { store, persistor } from './redux/store.js';
 import { theme } from './utils/extendTheme.js';
 import App from './App.jsx';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter basename='/'>
-        <App />
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={<Spinner />} persistor={persistor}>
+        <BrowserRouter basename='/'>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
     </ChakraProvider>
   </StrictMode>,
 );
