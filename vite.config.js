1d89b65bff9fc5586cefe42a8cb5cfe89303ecfc
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2024-09-29 20:06:08 +0300

    Add vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/path/to/index.html',
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
